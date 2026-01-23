28c128a269bc865bee04f0ad0d23c5592bf73641
Dev Torsson <devtorsson@Mac-mini.local>
2026-01-23 13:45:36 +0200

    Remove the animation for web section

--- app/services/web/page.tsx
+++ app/services/web/page.tsx
@@ -1,4 +1,4 @@
-import NexusHeroSection from "@/app/components/Backgrounds/NexusHeroSection";
+
 import Header from "@/app/components/Layout/Header";
 
 export default function WebServicesPage() {
@@ -6,9 +6,6 @@ export default function WebServicesPage() {
     <main>
       <Header />
 
-      <NexusHeroSection />
-      {/* next sections below */}
-      <section id="services-web" style={{ height: "0vh" }} />
     </main>
   );
 }
